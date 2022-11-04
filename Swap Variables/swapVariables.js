453ceb6957681455ee5b08d7f0f97f097d298fcc
AhmedWael1399 <ahmedeldeebany@gmail.com>
2022-11-05 00:38:20 +0200

    second-commit

---
+++ Swap Variables/swapVariables.js
@@ -0,0 +1,32 @@
+'use strict'
+const prompt = require("prompt-sync")();
+
+const numberArray = [];
+const size = prompt("Enter the size of the array: ");
+let intValue;
+let number1;
+let number2;
+
+for (let i = 0; i < size; i++) {
+    let value = prompt(`Enter value #${i + 1}: `);
+    intValue = parseInt(value);
+    numberArray.push(intValue);
+}
+
+function swapVariables(arr, index1, index2) {
+    let temp;
+    temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+number1 = parseInt(prompt("Enter the first number to be swapped: "));
+number2 = parseInt(prompt("Enter the second number to be swapped: "));
+
+if (numberArray.includes(number1) && numberArray.includes(number2)) {
+
+    swapVariables(numberArray, numberArray.indexOf(number1), numberArray.indexOf(number2))
+    console.log(numberArray);
+}
+else
+    console.log("Please enter a valid input 😒");
